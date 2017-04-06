8d96a62edd815fb120edb968673d307c8995b484
caleblareau <caleblareau@g.harvard.edu>
2017-04-05 20:02:55 -0400

    started the package

---
+++ R/conversion.R
@@ -0,0 +1,2 @@
+#' @include easyLift.R
+NULL
---
+++ R/easyLift.R
@@ -0,0 +1,10 @@
+#' easyLift: Efficient parallelized compression of Hi-C Data
+#'
+#' The easyLift package contains functions to quickly convert
+#' between different builds of the mouse and human genome. 
+#' Note: this doens't support cross-species crossover. 
+#' 
+#' @docType package
+#' @name easyLift
+#' @import methods
+NULL
